6fff39201afdbe0254cee185e6c18ab2d2bd5b69
avdarya <avdarya@gmail.com>
2025-06-18 14:26:06 +0700

    fix

--- app/database/users.py
+++ app/database/users.py
@@ -3,7 +3,7 @@ from typing import Iterable, Type, Sequence
 from fastapi import HTTPException
 from sqlmodel import Session, select
 from .engine import engine
-from ..models.User import User, UserCreate
+from ..models.User import User, UserCreate, UserUpdate
 
 
 def get_user(user_id: int) -> User | None:
@@ -23,12 +23,12 @@ def create_user(user: UserCreate) -> User:
         session.refresh(new_user)
         return new_user
 
-def update_user(user_id: int, user: User) -> User:
+def update_user(user_id: int, user: UserUpdate) -> type[User]:
     with Session(engine) as session:
         db_user = session.get(User, user_id)
         if not db_user:
             raise HTTPException(status_code=404, detail="User not found")
-        user_data = user.model_dump(exclude_unset=True)
+        user_data = user.model_dump(exclude_unset=True, mode="json")
         db_user.sqlmodel_update(user_data)
         session.add(db_user)
         session.commit()
